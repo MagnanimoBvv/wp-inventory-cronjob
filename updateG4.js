65fa22fce156e4e45186604b204fb65d20ac7d67
Alexis Contreras <contrerasalexis380@gmail.com>
2026-02-10 18:25:01 -0600

    update handles for g4

--- updateG4.js
+++ updateG4.js
@@ -2,6 +2,91 @@ const axios = require('axios');
 const xml2js = require('xml2js');
 const { getProductByHandle, updateInventory } = require('./shopifyFunctions');
 
+const handles = {
+    "moc-dai": "mochila-takayama-daily-pack-moc-dai",
+    "anf-lin": "anfora-link-anf-lin",
+    "vas-ven": "vaso-venti-vas-ven",
+    "set-pop": "",
+    "anf-cav": "anfora-cava-anf-cav",
+    "anf-tan": "anfora-tanker-anf-tan",
+    "ter-kri": "termo-krypton-ter-kri",
+    "ter-tin": "termo-tiny-ter-tin",
+    "anf-lit": "anfora-link-terra-anf-lit",
+    "vas-bis": "vaso-bistro-vas-bis",
+    "vas-bca": "vaso-bistro-candy-acero-vas-bca",
+    "anf-ele": "anfora-element-anf-ele",
+    "anf-sou": "anfora-soul-anf-sou",
+    "vas-bla": "vaso-termico-black-vas-bla",
+    "anf-sta": "anfora-stahl-anf-sta",
+    "moc-lok": "mochila-takayama-lok-moc-lok",
+    "moc-urb": "mochila-wagner-urban-moc-urb",
+    "moc-spa": "mochila-wagner-space-moc-spa",
+    "moc-rol": "mochila-takayama-roller-moc-rol",
+    "moc-zen": "mochila-takayama-zen-moc-zen",
+    "moc-mai": "mochila-takayama-maiko-moc-mai",
+    "moc-tas": "mochila-wagner-task-moc-tas",
+    "moc-atd": "mochila-takayama-atomik-deluxe-moc-atd",
+    "moc-rak": "mochila-takayama-rak-moc-rak",
+    "moc-tai": "mochila-takayama-taiko-moc-tai",
+    "moc-dia": "mochila-takayama-diagonal-moc-dia",
+    "moc-sac": "mochila-takayama-sack-moc-sac",
+    "fun-iiq": "funda-laptop-takyama-iq-fun-iiq",
+    "lin-med": "linterna-medic-lin-med",
+    "lin-san": "linterna-sante-lin-san",
+    "lib-pea": "libreta-peach-lib-pea",
+    "lib-tre": "libreta-trend-lib-tre",
+    "lib-fab": "",
+    "lib-skd": "libreta-skin-dots-lib-skd",
+    "lib-skb": "libreta-skin-box-lib-skb",
+    "lib-sks": "libreta-skin-soft-lib-sks",
+    "lib-pes": "libreta-peach-spiral-lib-pes",
+    "lib-bor": "libreta-border-lib-bor",
+    "lib-bob": "libreta-border-black-lib-bob",
+    "lib-ski": "libreta-skin-lib-ski",
+    "lib-smt": "libreta-skin-metallics-lib-smt",
+    "lib-smi": "libreta-skin-mini-lib-smi",
+    "lib-vel": "libreta-velvet-lib-vel",
+    "lib-bio": "libreta-bio-lemongrass-lib-bio",
+    "lib-rok": "libreta-rock-lib-rok",
+    "lib-pok": "libreta-pocket-lib-pok",
+    "car-skc": "carpeta-skin-congress-car-skc",
+    "tap-duo-lli": "",
+    "set-rec": "set-de-recaditos-set-rec",
+    "set-re2": "set-de-recaditos-2-set-re2",
+    "usb-tec": "",
+    "lum-al4": "power-bank-lumina-4-lum-al4",
+    "pce-tec": "porta-celular-tech-pce-tec",
+    "kbi-son": "boligrafo-koi-bio-tinta-negra-kbi-son",
+    "ebc-trn": "boligrafo-epic-colors-bio-tinta-negra-ebc-trn",
+    "kbi-trn": "boligrafo-koi-bio-trans-tinta-negra-kbi-trn",
+    "bug-sol": "boligrafo-buggy-bug-sol",
+    "col-tra": "boligrafo-colors-col-tra",
+    "jel-mlk": "boligrafo-jello-jel-mlk",
+    "jel-tra": "boligrafo-jello-jel-tra",
+    "jel-eli": "boligrafo-jello-elite-jel-eli",
+    "jel-zen": "boligrafo-jello-zen-jel-zen",
+    "jum-sol": "boligrafo-jumbo-max-jum-sol",
+    "lob-so2": "boligrafo-lobby-2-lob-so2",
+    "spl-sol": "boligrafo-splash-spl-sol",
+    "sur-pla": "boligrafo-surf-touch-sur-pla",
+    "due-sra": "boligrafo-duet-solida-tinta-roja-azul-due-sra",
+    "due-trn": "boligrafo-duet-trans-tinta-roja-negra-due-trn",
+    "dak-roc": "boligrafo-dakar-rock-dak-roc",
+    "sha-met": "boligrafo-sharp-sha-met",
+    "bol-met": "boligrafo-bold-bol-met",
+    "jet-met": "",
+    "ari-met": "boligrafo-aria-ari-met",
+    "cen-met": "boligrafo-century-cen-met",
+    "dak-met": "boligrafo-dakar-dak-met",
+    "dak-tou": "boligrafo-dakar-touch-dak-tou",
+    "imp-neg": "boligrafo-imperial-imp-neg",
+    "imp-sol": "boligrafo-imperial-imp-sol",
+    "iss-met": "boligrafo-insignia-ss-iss-met",
+    "kan-met": "boligrafo-kanji-kan-met",
+    "mdv-met": "boligrafo-mondavi-mdv-met",
+    "mdv-tou": "boligrafo-mondavi-touch-mdv-tou"
+};
+
 async function getG4Products() {
     const soapBody = `<?xml version="1.0" encoding="ISO-8859-1"?>
     <SOAP-ENV:Envelope
@@ -48,14 +133,11 @@ async function updateG4Products(locationId, selectedKeys) {
     const responseProducts = await getG4Products();
 
     for (const key of selectedKeys) {
-        if (['lib-fab', 'tap-duo-lli', 'usb-tec', 'jet-met'].includes(key)) continue;
+        if (['lib-fab', 'tap-duo-lli', 'usb-tec', 'jet-met', 'set-pop'].includes(key)) continue;
         const vendorVariants = responseProducts.filter(p => p.codigo_producto.startsWith(key));
-        const product = vendorVariants[0];
         try {
             // if (key !== 'BP-235') continue; // If para pruebas con un producto específico
-            let initString = ['Boligrafos de Plastico', 'Boligrafos de Metal'].includes(product.linea) ? 'Bolígrafo' :
-                product.linea === 'Electronico' ? 'Power Bank' : '';
-            const handle = `${initString} ${product.nombre_producto} ${key}`.trim().toLocaleLowerCase().replace(/[.,]/g, '').replace(/[\s/]+/g, '-'); // Quita comas y puntos y reemplaza espacios y diagonales
+            const handle = handles[key];
             const shopifyProduct = await getProductByHandle(handle);
 
             const shopifyVariants = shopifyProduct.variants.nodes;
